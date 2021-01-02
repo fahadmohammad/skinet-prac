3d6600ba2ed37f1aed5e25f7304378b90fc62766
Mohammad Fahad <fahadmo0hammad@gmail.com>
2021-01-02 11:59:05 +0600

    End of section 20

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -30,7 +30,9 @@ namespace Core.Entities.OrderAggregate
 
         public decimal GetTotal()
         {
-            return Subtotal + DeliveryMethod.Price;
+            if(DeliveryMethod != null)
+                return Subtotal + DeliveryMethod.Price;
+            return 0;
         }
     }
 }
